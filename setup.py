a35c66bca14f28d3b6a1145bb8d3f2e62b468730
f0rkz <f0rkz@f0rkznet.net>
2017-06-25 12:21:17 -0400

    Add requirements to the project

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+requires = ['requests==2.18.1']
 
 setup(name='pywordlist',
-      version='0.0.1',
+      version='0.0.2',
       description='Random phrase generator. https://xkcd.com/936/',
       url='http://github.com/f0rkz/pywordlist',
       author='f0rkz',
       author_email='f0rkz@f0rkznet.net',
       license='MIT',
       packages=['pywordlist'],
-      install_requires=[],
+      install_requires=[requires],
       zip_safe=True)
